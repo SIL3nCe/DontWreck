8a13118f889f23ca9cf52f81c3d1adb188c4db70
Lord-Asterius <loic.mathiot70@gmail.com>
2020-01-31 22:53:48 +0100

    Added WorldClickDestinationSetter

---
+++ Assets/Scripts/WorldClickDestinationSetter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WorldClickDestinationSetter : MonoBehaviour
+{
+    public delegate void OnClicked(Vector3 clickPosition);
+
+    OnClicked onClickedCallbacks;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(mouseRay, out hitInfo))
+            {
+                onClickedCallbacks?.Invoke(hitInfo.point);
+            }
+        }
+    }
+
+    public void AddOnClickedCallback(OnClicked onClicked)
+    {
+        onClickedCallbacks += onClicked;
+    }
+
+    public void RemoveOnClickedCallback(OnClicked onClicked)
+    {
+        onClickedCallbacks -= onClicked;
+    }
+
+    public void ClearOnClickedCallbacks()
+    {
+        onClickedCallbacks = null;
+    }
+}
