1f0f0a34a673a64177a01d6ee80ddc2c8167d8cc
Kranck <frizzy.rastay@gmail.com>
2020-02-01 23:41:10 +0100

    Basic management of resources and associated in game GUI

--- Assets/Scripts/GUILifeBarBillboarder.cs
+++ Assets/Scripts/GUILifeBarBillboarder.cs
@@ -1,16 +1,16 @@
-﻿using System.Collections;
+﻿/*
+   Project      : GGJ20-JambonBoursin
+   Author		: Yannis Beaux (Kranck)
+   Date		    : 01 / 02 / 2020
+   Description  : Manager for all the units
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GUILifeBarBillboarder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void LateUpdate()
     {
 		transform.forward = Camera.main.transform.forward;
