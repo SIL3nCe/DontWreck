3a045dc8db87315987fafe8bfcae5c99f248dc6d
Lord-Asterius <loic.mathiot70@gmail.com>
2020-02-01 15:25:11 +0100

    Added InteractableObject

---
+++ Assets/Scripts/Objects/InteractableObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Objects
+{
+    public class InteractableObject : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
