7762d1bf7e5563f53580f53955de2bed5372f9dc
Florian BAYLE <f.bayle@geoide.fr>
2020-02-01 17:55:14 +0100

    [+] add canvas start menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button PlayButton;
+    public Button CreditsButton;
+    public Button QuitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Add listener
+        PlayButton.onClick.AddListener(PlayAction);
+        CreditsButton.onClick.AddListener(CreditsAction);
+        QuitButton.onClick.AddListener(QuitAction);        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnHover()
+    {
+
+    }
+
+
+
+    // Play action
+    void PlayAction()
+    {
+        Debug.Log("PLAY ACTION");
+    }
+
+    // Credits action
+    void CreditsAction()
+    {
+        Debug.Log("CREDITS ACTION");
+    }
+
+
+    // Quit action
+    void QuitAction()
+    {
+        Application.Quit();
+    }
+
+}
