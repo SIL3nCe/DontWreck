7ea5db1c63a167809a42a8748f9fc9f4a57405f2
Florian BAYLE <f.bayle@geoide.fr>
2020-02-01 19:37:28 +0100

    Start menu
    [+] animations

---
+++ Assets/Scripts/CreditsMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CreditsMenu : MonoBehaviour
+{
+
+    public CinemachineVirtualCamera MainCamera;
+    public CinemachineVirtualCamera CreditsCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void DisplayMainMenu()
+    {
+        GameObject.Find("MainMenu").GetComponent<Canvas>().enabled = true; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameObject.SetActive(false);
+            CreditsCamera.gameObject.SetActive(false);
+            MainCamera.gameObject.SetActive(true);
+
+            Invoke("DisplayMainMenu", 3);
+        }
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Cinemachine;
 
 public class MainMenu : MonoBehaviour
 {
     public Button PlayButton;
     public Button CreditsButton;
     public Button QuitButton;
+
+    public CinemachineVirtualCamera FirstCamera;
+    public CinemachineVirtualCamera PlayCamera;
+    public CinemachineVirtualCamera CreditsCamera;
+
+    public GameObject CreditsMenu;
 
     // Start is called before the first frame update
     void Start()
@@ -30,18 +37,34 @@ public class MainMenu : MonoBehaviour
 
     }
 
-
+    IEnumerator ShowCreditsMenu()
+    {
+        yield return new WaitForSeconds(5);
+        CreditsMenu.SetActive(true);
+    }
 
     // Play action
     void PlayAction()
     {
         Debug.Log("PLAY ACTION");
+
+        GetComponent<Canvas>().enabled = false;
+        FirstCamera.gameObject.SetActive(false);
+        PlayCamera.gameObject.SetActive(true);
     }
 
     // Credits action
     void CreditsAction()
     {
         Debug.Log("CREDITS ACTION");
+        StartCoroutine(ShowCreditsMenu());
+        GetComponent<Canvas>().enabled = false;
+        FirstCamera.gameObject.SetActive(false);
+        PlayCamera.gameObject.SetActive(false);
+        CreditsCamera.gameObject.SetActive(true);
+        
+        
+
     }
 
 
