337e8fa725b3474e08d342c6e5dd8a4956be0849
Kranck <frizzy.rastay@gmail.com>
2020-02-02 16:53:19 +0100

    Added sounds for gathering resources. Modified how resource are gathered

--- Assets/Scripts/Objects/InteractableFire.cs
+++ Assets/Scripts/Objects/InteractableFire.cs
@@ -23,11 +23,11 @@ namespace Objects
 
 		public void Update()
 		{
-			if ((Time.time - m_startTime) > 5f)
-			{
-				m_startTime = Time.time;
-				GameManager.m_instance.m_resourcesManager.DecreasePlayerLifePoints(10);
-			}
+			//if ((Time.time - m_startTime) > 5f)
+			//{
+			//	m_startTime = Time.time;
+			//	GameManager.m_instance.m_resourcesManager.DecreasePlayerLifePoints(10);
+			//}
 		}
 
 		private void CrewInteract()
