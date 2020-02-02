7567457b92c9a35d7345b799d6ece5cdb1f5b3c7
Stephane <stephane.perrez@gmail.com>
2020-02-02 15:19:20 +0100

    Updated first Island

---
+++ Assets/Scripts/IslandManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IslandManager : MonoBehaviour
+{
+    public Tide seawater;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seawater.AddOnTideUpCallback(OnTideUp);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTideUp()
+    {
+        //GameManager.EndLevel();
+    }
+}
