47fdb3fac074c59e6050f8dc9e3353ea9175b0bb
Lord-Asterius <loic.mathiot70@gmail.com>
2020-02-02 14:09:07 +0100

    All boat objects are now destroyable

---
+++ Assets/Scripts/Objects/InteractableBoatObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Objects
+{
+    public class InteractableBoatObject : Objects.InteractableObject
+    {
+        public new void Start()
+        {
+            base.Start();
+        }
+
+        void Update()
+        {
+
+        }
+
+        public override void Interact(GameObject unit)
+        {
+            if (unit.GetComponent<Crew.CrewController>() != null)
+            {
+                CrewInteract();
+            }
+            else if (unit.GetComponent<EnemyController>() != null)
+            {
+                EnnemyInteract();
+            }
+        }
+
+        private void CrewInteract()
+        {
+        }
+
+        private void EnnemyInteract()
+        {
+            ModHp(-10);
+        }
+    }
+}
