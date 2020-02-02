185e18ae47ad273c140992da4dc17351c1b3b757
Kranck <frizzy.rastay@gmail.com>
2020-02-02 16:29:30 +0100

    Added damages with the fire.

--- Assets/Scripts/Objects/InteractableFire.cs
+++ Assets/Scripts/Objects/InteractableFire.cs
@@ -6,6 +6,8 @@ namespace Objects
 {
     public class InteractableFire : Objects.InteractableObject
     {
+		private float m_startTime;
+
         public new void Start()
         {
             base.Start();
@@ -19,7 +21,16 @@ namespace Objects
             }
         }
 
-        private void CrewInteract()
+		public void Update()
+		{
+			if ((Time.time - m_startTime) > 5f)
+			{
+				m_startTime = Time.time;
+				GameManager.m_instance.m_resourcesManager.DecreasePlayerLifePoints(10);
+			}
+		}
+
+		private void CrewInteract()
         {
             ModHp(-10);
             if (m_hp <= 0)
