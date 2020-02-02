3650fe8668befbdcfb0310eb7b173a75d280dfbf
Thomas <tom.pradal@aliceadsl.fr>
2020-02-02 14:51:55 +0100

    Added fire icon handle. Fixed repaid icon on cannon

---
+++ Assets/Scripts/Objects/InteractableFire.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Objects
+{
+    public class InteractableFire : Objects.InteractableObject
+    {
+        public new void Start()
+        {
+            base.Start();
+        }
+
+        public override void Interact(GameObject unit)
+        {
+            if (unit.GetComponent<Crew.CrewController>() != null)
+            {
+                CrewInteract();
+            }
+        }
+
+        private void CrewInteract()
+        {
+            ModHp(-10);
+        }
+    }
+}
